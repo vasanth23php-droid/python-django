4bec8257eb363a893f37067bab6ffa3dc7014f13
unknown <vasanth.23php@gmail.com>
2025-12-21 18:01:03 +0530

    Added code specfic element only

--- movieinfo/movielist_app/urls.py
+++ movieinfo/movielist_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from movielist_app.views import movie_list
+from movielist_app.views import movie_list,movie_details
 
 urlpatterns = [
         path('list/', movie_list, name='Movie-list'),
+        path('<int:id>', movie_details, name='Movie-list'),
     ]
--- movieinfo/movielist_app/views.py
+++ movieinfo/movielist_app/views.py
@@ -6,3 +6,11 @@ def movie_list(request):
     Movies = Movie.objects.all()
     data   = {"Movies" : list(Movies.values())}
     return JsonResponse(data)
+def movie_details(request, id):
+    movie = Movie.objects.get(id=id)
+    data = {
+        'name' : movie.name,
+        'description' : movie.description,
+        'activate' : movie.activate
+    }
+    return JsonResponse(data)
